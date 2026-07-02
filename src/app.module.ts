538d882b39102fa4b3dc462d0df96d45e2a6c83f
pedrok <pedrokimur@hotmail.com>
2026-07-02 16:54:43 -0300

    chore: configure TypeORM with OracleDB and Swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'oracle',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        sid: configService.get<string>('DB_SID'),
+        synchronize: false,
+        logging: ['query', 'error'],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
