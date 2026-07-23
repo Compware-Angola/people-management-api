546e5bb3102c7fe97c52974dcd1482506b9b56ba
Domingos Canhanga <canhanga96@gmail.com>
2026-07-23 15:06:48 +0100

    refactor: consolidate user service logic and update module structure for better maintainability

--- src/modules/user/users.module.ts
+++ src/modules/user/users.module.ts
@@ -1,25 +1,13 @@
-import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-
-import { UsersService } from './services/users.service'
-
-import { TypeormUserRepository } from './repositories/typeorm-user.repository'
-import { UserEntity } from './entity/user.entity'
-import { UserRepository } from './repositories/user-repository'
-import { UsersController } from './controllers/users.controller'
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-
-  controllers: [UsersController],
-
-  providers: [
-    UsersService,
-
-    {
-      provide: UserRepository,
-      useClass: TypeormUserRepository,
-    },
-  ],
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
-export class UsersModule {}
+export class UserModule {}
